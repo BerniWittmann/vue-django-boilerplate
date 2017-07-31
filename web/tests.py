0f08f1b2a636ede297bf1a62746b765581625d16
BerniWittmann <wittmann_b@web.de>
2017-07-31 21:00:13 +0200

    added tests for web view

--- web/tests.py
+++ web/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+class HomeTestCase(TestCase):
+    def test_page_is_served(self):
+        client = Client()
+        response = client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<html>', status_code=200)
+        self.assertContains(response, '<head>', status_code=200)
+        self.assertContains(response, '<body>', status_code=200)
+        self.assertContains(response, '</html>', status_code=200)
+        self.assertContains(response, '</head>', status_code=200)
+        self.assertContains(response, '</body>', status_code=200)
+
+    def test_page_is_served_sub_url(self):
+        client = Client()
+        response = client.get(reverse('home') + 'sub_page/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<html>', status_code=200)
+        self.assertContains(response, '<head>', status_code=200)
+        self.assertContains(response, '<body>', status_code=200)
+        self.assertContains(response, '</html>', status_code=200)
+        self.assertContains(response, '</head>', status_code=200)
